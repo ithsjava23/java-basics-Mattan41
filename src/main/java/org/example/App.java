def98b33539f6441844156a41ba386f8de36775c
Mattan41 <Mats.kruskopf@gmail.com>
2023-09-05 17:46:30 +0200

    added menu with choices

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,53 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello There!");
+
+        Scanner sc = new Scanner(System.in);
+        String choice = "";
+
+        while (!choice.equalsIgnoreCase("e")){
+            printMenu();
+            choice = sc.nextLine();
+            switch (choice) {
+                case "1" -> inputElectricityPrice();
+                case "2" -> minMaxAverageValue();
+                case "3" -> sortElectricityPrices();
+                case "4" -> bestTimeToCharge();
+                case "5" -> visualizeElectricityPrices();
+        }
+        }
+
     }
+
+    public static void printMenu() {
+        System.out.println("Elpriser\n" +
+                "========\n" +
+                "1. Inmatning\n" +
+                "2. Min, Max och Medel\n" +
+                "3. Sortera\n" +
+                "4. Bästa Laddningstid (4h)\n" +
+                "e. Avsluta");
+    }
+
+    public static void inputElectricityPrice() {
+    //ToDo: input method
+    }
+    public static void minMaxAverageValue(){
+    //ToDo: minMaxMethod method
+    }
+    public static void sortElectricityPrices(){
+        //ToDo: sorting Method
+    }
+    public static void bestTimeToCharge(){
+        //ToDo: charge time method
+    }
+
+    public static void visualizeElectricityPrices(){
+        //ToDo: Visualisation method
+    }
+    public static void endProgram(){}
 }
