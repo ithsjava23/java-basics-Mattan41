1eee10b61f65d6311c7312df6b0acd13ce278c3d
Mattan41 <Mats.kruskopf@gmail.com>
2023-09-05 18:08:23 +0200

    updated printMenu to textblock and changed from println to print

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -30,7 +30,7 @@ public class App {
                 1. Inmatning
                 2. Min, Max och Medel
                 3. Sortera
-                4. Bästa Laddningstid (4h)
+                4. Bästa Laddningstid (4h)              
                 e. Avsluta
                 """);
     }
