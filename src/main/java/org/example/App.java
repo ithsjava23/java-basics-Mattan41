cfe8be9b5fd446f70f6f28f2ddecb0db9fc9d43c
Mattan41 <Mats.kruskopf@gmail.com>
2023-09-06 10:07:16 +0200

    updated input electricity prices

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,17 +8,19 @@ public class App {
 
         Scanner sc = new Scanner(System.in);
         String choice = "";
+        int[] electricityPrice = new int[24];
 
         while (!choice.equalsIgnoreCase("e")){
             printMenu();
             choice = sc.nextLine();
             switch (choice) {
-                case "1" -> inputElectricityPrice();
+                case "1" -> inputElectricityPrice(electricityPrice, sc);
                 case "2" -> minMaxAverageValue();
                 case "3" -> sortElectricityPrices();
                 case "4" -> cheapestTimeToCharge();
                 case "5" -> visualizeElectricityPrices();
         }
+
         }
 
     }
@@ -30,26 +32,30 @@ public class App {
                 1. Inmatning
                 2. Min, Max och Medel
                 3. Sortera
-                4. Bästa Laddningstid (4h)              
+                4. Bästa Laddningstid (4h)
                 e. Avsluta
                 """);
     }
 
-    public static void inputElectricityPrice() {
-    //ToDo: input method
+    private static void inputElectricityPrice(int[] electricityPrice, Scanner sc) {
+        for (int i = 0; i < electricityPrice.length; i++) {
+            System.out.println("Ange elpris i öre för timme " + i + "-" + (i + 1) + ":");
+            electricityPrice[i] = sc.nextInt();
+        }
     }
+
     public static void minMaxAverageValue(){
     //ToDo: minMaxMethod method
     }
     public static void sortElectricityPrices(){
-        //ToDo: sorting Method
+        //ToDo: sorting Method + print
     }
     public static void cheapestTimeToCharge(){
         //ToDo: charge time method
     }
 
     public static void visualizeElectricityPrices(){
-        //ToDo: Visualisation method
+        //ToDo: Visualisation method + add menu choice
     }
 
 }
