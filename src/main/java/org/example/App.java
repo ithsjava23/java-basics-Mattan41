35fb15b1ed590ada7e41e6a639b4b88f8d9e240d
Mattan41 <Mats.kruskopf@gmail.com>
2023-09-07 14:34:23 +0200

    added cheapest charging hours

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,7 +2,6 @@ package org.example;
 
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Scanner;
 
 public class App {
@@ -25,10 +24,11 @@ public class App {
                 }
 
                 case "3" -> sortElectricityPrices(electricityPrice);
-                case "4" -> cheapestTimeToCharge();
+                case "4" -> cheapestTimeToCharge(electricityPrice);
                 case "5" -> visualizeElectricityPrices();
             }
         }
+
 
     }
 
@@ -56,7 +56,7 @@ public class App {
 
     public static void maxValue(int[][] electricityPrice) {
         int max = Integer.MIN_VALUE;
-        int maxRow = -1;
+        int maxRow;
         for (int i = 0; i < electricityPrice.length; i++) {
             if (electricityPrice[i][1] > max) {
                 max = electricityPrice[i][1];
@@ -71,13 +71,14 @@ public class App {
         }
 
     }
+
     public static void minValue(int[][] electricityPrice) {
         int min = Integer.MAX_VALUE;
-        int minRow = -1;
+        int minRow;
         for (int i = 0; i < electricityPrice.length; i++) {
             if (electricityPrice[i][1] < min) {
                 min = electricityPrice[i][1];
-                ;
+
             }
         }
         for (int i = 0; i < electricityPrice.length; i++) {
@@ -89,14 +90,10 @@ public class App {
         }
     }
     public static void averageValue(int[][] electricityPrice) {
-        double sum = 0;
-        for (int i = 0; i < electricityPrice.length; i++) {
-            sum += electricityPrice[i][1];
-        }
-        double average = sum / electricityPrice.length;
+        double average = getAverageDouble(electricityPrice);
+        //float getAverage(electricityPrice);
         System.out.print("Medelpris: "+ average + " öre/kWh\n \n");
     }
-
     public static void sortElectricityPrices(int[][] electricityPrice) {
         for (int i = 0; i < electricityPrice.length; i++) {
             for (int j = i + 1; j < electricityPrice.length; j++) {
@@ -112,11 +109,122 @@ public class App {
             System.out.println(time + " " + electricityPrice[i][1] + " öre");
         }
     }
-        //Arrays.sort(electricityPrice, Comparator.comparingInt(a -> a[1])); // sort array method
 
-    public static void cheapestTimeToCharge(){
-        //ToDo: best charge time method
+    //Arrays.sort(electricityPrice, Comparator.comparingInt(a -> a[1])); // sort array method
+
+    public static void cheapestTimeToCharge(int[][] electricityPrice){
+
+        int[][] cheapCharge = new int[4][2];
+
+            int max = Integer.MAX_VALUE;
+            for (int i = 0; i < cheapCharge.length; i++) {
+                Arrays.fill(cheapCharge[i], max);
+            }
+
+        int[][] tempArray = new int[cheapCharge.length][cheapCharge[0].length];
+
+            for (int i = 0; i < cheapCharge.length; i++) {
+                tempArray[i] = cheapCharge[i].clone();
+            }
+
+        /*for (int row = 0; row < electricityPrice.length - 3; row++) {
+            System.arraycopy(electricityPrice, row, tempArray, 0, tempArray.length);
+
+                int sum1 = 0;
+                int sum2 = 0;
+
+                for (int i = 0; i < cheapCharge.length; i++) {
+                    sum1 += cheapCharge[i][1];
+                    sum2 += tempArray[i][1];
+                }
+                    if (sum2 < sum1) {
+                        System.arraycopy(tempArray, row , cheapCharge, 0, tempArray.length);
+                    }
+        }
+
+         */
+
+
+        for (int row = 0; row < (electricityPrice.length - 3); row++) {
+            for (int i = 0; i < 4; i++) {
+
+                for (int j = 0; j < 4; j++) {
+                    tempArray[j][0] = electricityPrice[j + row][0];
+                    tempArray[j][1] = electricityPrice[j + row][1];
+                }
+
+            }
+            int sum1 = 0;
+            int sum2 = 0;
+
+            for (int k = 0; k < cheapCharge.length; k++) {
+                sum1 += cheapCharge[k][1];
+                sum2 += tempArray[k][1];
+
+                if (sum2 < sum1) {
+                    for (int l = 0; l < 4; l++) {
+                        cheapCharge[l][0] = tempArray[l][0];
+                        cheapCharge[l][1] = tempArray[l][1];
+                    }
+                }
+            }
+
+        }
+
+
+
+        double average = getAverageDouble(cheapCharge);
+        String time = String.format("%02d", cheapCharge[0][0]);
+            System.out.println("påbörja laddning: " + time + ", medelpris 4h: "+ average + " öre /kWh\n \n");
+
+
+
+/*
+
+        for (int i = 0; i < electricityPrice.length - 4; i++) {
+
+                for (int i = 0; i < 4; i++) {
+                    for (int j = 0; j < 2; j++) {
+                        int cheapCharge[i][j] = electricityPrice[i * 6 + j];
+                    }
+                }
+
+ */
+
+
     }
+    public static double getAverageDouble(int[][] electricityPrice) {
+        double sum = 0;
+        for (int i = 0; i < electricityPrice.length; i++) {
+            sum += electricityPrice[i][1];
+        }
+        return sum / electricityPrice.length;
+    }
+    public static int sumArray(int [][] tempArray) {
+        int sum = 0;
+
+        for (int i = 0; i < tempArray.length; i++) {
+            sum += tempArray[i][0];
+        }
+        return sum;
+    }
+    public static double getAverage(int[][] cheapCharge) {
+        double sum = 0;
+        for (int i = 0; i < cheapCharge.length; i++) {
+            sum += cheapCharge[i][1];
+        }
+        return sum / cheapCharge.length;
+    }
+
+    public static double getAverageD(int[][] tempArray) {
+        double sum = 0;
+        for (int i = 0; i < tempArray.length; i++) {
+            sum += tempArray[i][1];
+        }
+        return sum / tempArray.length;
+    }
+
+
     public static void visualizeElectricityPrices(){
         //ToDo: Visualisation method + add menu choice
     }
